b0342661a1ad2ab229ba59d160b7dc393488e79a
Hewan Redie <tewol22@gmail.com>
2021-07-19 20:19:13 -0500

    Database models created

--- models/index.js
+++ models/index.js
@@ -5,12 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
+  foreignKey: 'id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product-tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product-tag'
+});
 
 module.exports = {
   Product,
